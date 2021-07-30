d608cf7723a6f52416cc013bd94c42332670785c
Zander Ambrose <alexander.d.ambrose@gmail.com>
2021-07-30 16:12:36 -0400

    refactor code and add php to submit email in forms

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['mailFrom'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "alexander.d.ambrose@gmail.com";
+    $headers = "From: ". $mailFrom; 
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+    
+    mail($mailTo. $subject, $txt, $headers);
+    header("Location: index.html"); 
+}
+
